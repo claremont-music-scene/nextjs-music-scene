484cb51d2ee990c126ef1ce940d74e61813898ba
Brian Luft <brian@lincolnloop.com>
2020-12-28 15:37:46 -0800

    Content for volunteer page

--- pages/community/volunteering/index.js
+++ pages/community/volunteering/index.js
@@ -1,5 +1,118 @@
 import SingleColumnLayout from "../../../components/layouts/single-column";
 
-export default function Volunteering(){
-    return <SingleColumnLayout>volunteering</SingleColumnLayout>
+export default function Volunteering() {
+    return (<SingleColumnLayout>
+
+        <section>
+            <header className="main"><h2>About Volunteering</h2></header>
+            <p><span className="image right"><span className="image fit"><img srcSet="/community/volunteering/vol-lunch_hu20a2bc6ebbfd3d2c7b9e766884531d94_1453577_320x0_resize_q75_box.jpg 320w,
+/community/volunteering/vol-lunch_hu20a2bc6ebbfd3d2c7b9e766884531d94_1453577_640x0_resize_q75_box.jpg 640w,
+/community/volunteering/vol-lunch_hu20a2bc6ebbfd3d2c7b9e766884531d94_1453577_768x0_resize_q75_box.jpg 768w,
+/community/volunteering/vol-lunch_hu20a2bc6ebbfd3d2c7b9e766884531d94_1453577_1024x0_resize_q75_box.jpg 1024w,
+/community/volunteering/vol-lunch_hu20a2bc6ebbfd3d2c7b9e766884531d94_1453577_1366x0_resize_q75_box.jpg 1366w,
+/community/volunteering/vol-lunch_hu20a2bc6ebbfd3d2c7b9e766884531d94_1453577_1600x0_resize_q75_box.jpg 1600w,
+/community/volunteering/vol-lunch_hu20a2bc6ebbfd3d2c7b9e766884531d94_1453577_1920x0_resize_q75_box.jpg 1920w"
+                                                                              sizes="88vw"
+                                                                              src="/community/volunteering/vol-lunch_hu20a2bc6ebbfd3d2c7b9e766884531d94_1453577_640x0_resize_q75_box.jpg"/></span></span>
+                The Claremont Music Scene is made possible by a network of like-minded people collaborating
+                together. This is a central hub where
+                you can find and connect with others around shared interests. Maybe you've considered hosting a jam
+                session. Perhaps you have
+                an idea for teaching a class: beginning synth, or make your own junkyard intruments, or learning
+                rhythm via Steve Reich's
+                Clapping Music. Yes, we can use more drum circles, children's marching band clubs, and listening
+                parties. How about getting a few
+                friends together in the park for some Parkaoke - open-air karaoke?
+                Let's go out and create the things we want to see, find places to be creative, and lead by example.
+            </p><h2 id="want-to-help">Want to Help?</h2><p>We welcome help and support from any person that acting
+            in a positive and inclusive direction. If you’ve done something or
+            are working on something related to music in our area, let us know and we will do our best to recognize
+            and share your efforts to the wider
+            community. We also have a more formalized volunteer program that encourages community members to work on
+            designated projects
+            related to directed efforts within the music scene.</p><h3 id="music-scene-volunteers">Music Scene
+            Volunteers</h3><p>Volunteers propose a project to work on for something they’re interested in. (We can
+            help) You’re encouraged to own it and make it
+            your own experience. There’s no hard commitment but if you can average around one hour per month and
+            share a brief update of
+            your work you’ll be eligible as an official volunteer. Volunteers will receive or have access to:</p>
+            <ul>
+                <li>Seasonal catered meals with the volunteer group</li>
+                <li>A profile page on the website for highlighting involvement and achievements</li>
+                <li>A stylish Claremont Music Scene T-shirt</li>
+                <li>Stickers, decals, and other swag</li>
+            </ul>
+            <section><h3 className="volunteering">Volunteer Project Ideas</h3>
+                <div className="features volunteering">
+                    <article><span className="icon fa-newspaper"></span>
+                        <div className="content"><h3><Link href="/community/volunteering/website/"><a>Newsroom</a></Link></h3>
+                            <p>Help with production and maintenance of the website</p><Link className="button"
+                                                                                         href="/community/volunteering/website/"><a>Learn
+                                More</a></Link></div>
+                    </article>
+                    <article><span className="icon fa-calendar"></span>
+                        <div className="content"><h3><Link
+                            href="/community/volunteering/event-planning-and-hosting/"><a>Events</a></Link></h3><p>Good for
+                            planners and organizers. Also if you have creative space available at your business,
+                            venue, or property</p><Link className="button"
+                                                     href="/community/volunteering/event-planning-and-hosting/"><a>Learn
+                            More</a></Link></div>
+                    </article>
+                    <article><span className="icon solid fa-graduation-cap"></span>
+                        <div className="content"><h3><Link href="/community/volunteering/teaching/"><a>Teaching</a></Link></h3>
+                            <p>Create avenues for learning to complement existing community offerings</p><Link
+                                className="button" href="/community/volunteering/teaching/"><a>Learn More</a></Link></div>
+                    </article>
+                    <article><span className="icon solid fa-people-carry"></span>
+                        <div className="content"><h3><a href="/community/volunteering/sharing/">Sharing</a></h3>
+                            <p>Financial Generosity and Community Outreach</p><Link className="button"
+                                                                                 href="/community/volunteering/sharing/"><a>Learn
+                                More</a></Link></div>
+                    </article>
+                </div>
+            </section>
+            <h2 id="getting-started">Getting Started</h2><p>To become a volunteer, we ask that you attend a
+            Volunteer Orientation Meeting. These are casual Zoom meetings that occur a few times
+            per month at different times and days of the week in hopes of accommodating people’s differing
+            schedules.</p><p>Meetings will last for a maximum of 30 minutes; many may be shorter depending on
+            attendance and extent of open discussion.</p><h3 id="orientation-meeting-schedule">Orientation Meeting
+            Schedule</h3>
+            <div className="row">
+                <div className="col-6 col-12-medium">
+                    <dl>
+                        <dt>Monday Lunchtime Edition</dt>
+                        <dd><p><i>You hadn't gotten any work done yet anyway</i><br/><b>Next Meeting:</b> Monday
+                            Sept. 14, 12pm</p></dd>
+                        <dt>Tuesday Evening Edition</dt>
+                        <dd><p><i>More fun than a political election</i><br/><b>Next Meeting:</b> Tuesday Sept. 15,
+                            8pm</p></dd>
+                        <dt>Sunday Afternoon Edition</dt>
+                        <dd><p><i>Less commerials than the sports show</i><br/><b>Next Meeting:</b> Sunday Sept. 13,
+                            3pm</p></dd>
+                    </dl>
+                </div>
+                <div className="col-6 col-12-medium">
+                    <div className="box"><p>Sign up to receive access information for
+                        the <Link href="/community/volunteering/#orientation-meeting-schedule"><a>Volunteer Zoom
+                            Meetings</a></Link>. <i>Replies are not automatic and may take up to a few days.</i></p>
+                        <form method="post" name="zoom-signup"><input type="hidden" name="form-name"
+                                                                      value="zoom-signup"/>
+                            <div className="row gtr-uniform">
+                                <div className="col-12"><input type="email" name="demo-email" id="demo-email"
+                                                               placeholder="Email"
+                                />
+                                </div>
+                                <div className="col-12">
+                                    <ul className="actions">
+                                        <li><input type="submit"
+                                                   value="Send Me Meeting Details" className="primary"/></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </SingleColumnLayout>)
 }

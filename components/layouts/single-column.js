78d81f8c939b0665a26dbea355e0309aaaa6927f
Ben Brooks <ben@nutandem.com>
2021-01-01 09:53:48 -0800

    hotfix: warning about unique keys in single-column

--- components/layouts/single-column.js
+++ components/layouts/single-column.js
@@ -2,9 +2,9 @@ import HeaderSocialLinks from "../header-social-links";
 import Link from "next/link";
 import Nav from "../nav";
 import Footer from "../footer";
-import {useRouter} from "next/router";
+import { useRouter } from "next/router";
 
-export default function SingleColumnLayout({children}) {
+export default function SingleColumnLayout({ children }) {
     const router = useRouter()
     const parts = router.pathname.split('/')
     let crumbs = []
@@ -16,11 +16,11 @@ export default function SingleColumnLayout({children}) {
         <div id="wrapper">
             <div id="main">
                 <div className="inner">
-                    <HeaderSocialLinks/>
+                    <HeaderSocialLinks />
                     <p className="nav navmenu">
                         <Link href="/"><a>Home</a></Link>
-                        {crumbs.map((crumb) => {
-                            return (<span>&nbsp;
+                        {crumbs.map((crumb, idx) => {
+                            return (<span key={idx}>&nbsp;
                                 <Link href={`/${crumb}/`}>
                                     <a>{crumb}</a>
                                 </Link></span>
@@ -32,8 +32,8 @@ export default function SingleColumnLayout({children}) {
             </div>
             <div id="sidebar" className="inactive">
                 <div className="inner">
-                    <Nav/>
-                    <Footer/>
+                    <Nav />
+                    <Footer />
                 </div>
                 <a href="#sidebar" className="toggle">Toggle</a>
             </div>

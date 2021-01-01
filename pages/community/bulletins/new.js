77832540de7f66872d7b0cbf0e1d9ee0697e61af
Ben Brooks <ben@nutandem.com>
2021-01-01 12:02:50 -0800

    npm i react-hook-forms; make basic form/validation

---
+++ pages/community/bulletins/new.js
@@ -0,0 +1,30 @@
+import SingleColumnLayout from '../../../components/layouts/single-column';
+
+import { useForm } from 'react-hook-form';
+
+const NewBulletin = () => {
+    const { register, handleSubmit, errors } = useForm(),
+        onSubmit = (data) => {
+            console.log(data);
+        };
+
+    return (
+        <SingleColumnLayout>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div>
+                    <label htmlFor='title'>Title</label>
+                    <input name='title' id='title' ref={register({ required: true })} />
+                    <div className="field-error">{errors.title && 'A post title is required.'}</div>
+                </div>
+                <div>
+                    <label htmlFor='content'>Content</label>
+                    <input name='content' id='content' ref={register({ required: true })} />
+                    <div className="field-error">{errors.content && 'Please enter some content for your post.'}</div>
+                </div>
+                <input type='submit' />
+            </form>
+        </SingleColumnLayout>
+    );
+}
+
+export default NewBulletin;

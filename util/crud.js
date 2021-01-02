b5d5aec4bc8b8997f7065b52ca10f164d72c2b6e
Ben Brooks <ben@nutandem.com>
2021-01-02 08:19:46 -0800

    attempting different POSTs

--- util/crud.js
+++ util/crud.js
@@ -1,30 +1,55 @@
+import Router from 'next/router';
+
 export const fetcher = async (...args) => fetch(...args).then(res => res.json());
 
 export const poster = async ({ title, content }) => {
-    if (!title || !content) { return; }
+    // if (!title || !content) { return; }
 
-    fetch(
-        'https://music-scene-api.herokuapp.com/api/bulletin_board/items/',
-        {
+    try {
+        const body = {
+            "title": title,
+            "content": content,
+            "meta": null
+        };
+        console.log('🚀 ~ poster ~ body', body);
+        console.log('🚀 ~ poster ~ JSON.stringify(body)', JSON.stringify(body));
+
+        const res = await fetch(`https://music-scene-api.herokuapp.com/api/bulletin_board/items/`, {
             method: 'POST',
             mode: 'no-cors',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                "title": 'asdf',
-                "content": 'qwer',
-                "meta": null
-            })
-        }
-    ).then(response => {
-        if (response.ok) {
-            response.json().then(json => {
-                console.log(json);
-            })
-        }
-    }).catch(err => {
-        console.log('error during post:', err);
-    });
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(body),
+        });
+        const data = await res.json();
+        console.log('🚀 ~ poster ~ data', data);
+        await Router.push('/');
+    }
+    catch (error) {
+        console.error(error);
+    }
+
+    // fetch(
+    //     'https://music-scene-api.herokuapp.com/api/bulletin_board/items/',
+    //     {
+    //         method: 'POST',
+    //         mode: 'no-cors',
+    //         headers: {
+    //             'Accept': 'application/json',
+    //             'Content-Type': 'application/json',
+    //         },
+    //         body: JSON.stringify({
+    //             "title": title,
+    //             "content": content,
+    //             // "meta": null
+    //         })
+    //     }
+    // ).then(response => {
+    //     if (response.ok) {
+    //         response.json().then(json => {
+    //             console.log(json);
+    //         })
+    //     }
+    // }).catch(err => {
+    //     console.log('error during post:', err);
+    // });
 }

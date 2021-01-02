8d6a5e3d2a00ede667b36d9f715e0809952cf028
Ben Brooks <ben@nutandem.com>
2021-01-02 08:44:50 -0800

    more post attempts, cookies

--- util/crud.js
+++ util/crud.js
@@ -2,6 +2,26 @@ import Router from 'next/router';
 
 export const fetcher = async (...args) => fetch(...args).then(res => res.json());
 
+
+
+
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+
 export const poster = async ({ title, content }) => {
     // if (!title || !content) { return; }
 
@@ -14,15 +34,22 @@ export const poster = async ({ title, content }) => {
         console.log('🚀 ~ poster ~ body', body);
         console.log('🚀 ~ poster ~ JSON.stringify(body)', JSON.stringify(body));
 
+        let csrftoken = getCookie('csrftoken');
+
         const res = await fetch(`https://music-scene-api.herokuapp.com/api/bulletin_board/items/`, {
             method: 'POST',
             mode: 'no-cors',
-            headers: { 'Content-Type': 'application/json' },
+            credentials: "same-origin",
+            // headers: {
+            //     'Accept': 'application/json, text/plain, */*',
+            //     'Content-Type': 'application/json',
+            //     "X-CSRFToken": csrftoken
+            // },
             body: JSON.stringify(body),
         });
-        const data = await res.json();
-        console.log('🚀 ~ poster ~ data', data);
-        await Router.push('/');
+        // const data = await res.json();
+        // console.log('🚀 ~ poster ~ data', data);
+        // await Router.push('/');
     }
     catch (error) {
         console.error(error);

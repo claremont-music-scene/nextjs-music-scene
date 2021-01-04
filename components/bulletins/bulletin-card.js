f05aaee1448ed314885e318a6edba27d0cb61664
Brian Luft <brian@lincolnloop.com>
2021-01-04 15:43:44 -0800

    Bulletin cards can use 'image_only' option if present in meta field

--- components/bulletins/bulletin-card.js
+++ components/bulletins/bulletin-card.js
@@ -4,17 +4,19 @@ export default function BulletinCard({ bulletin }) {
     const bulletinClass = 'grid-cell' + (bulletin.is_featured ? ' feat-' + bulletin.featured_order : ''),
         content = bulletin.content.replace(/\n/g, '<br/>');
 
+    const contentSection = (<div>
+        <div>
+            <Link href={'/community/bulletins/' + bulletin.id}>
+                <a className={'title'}>{bulletin.title}</a>
+            </Link>
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: content }} />
+    </div>)
+
     return (
         <div key={bulletin.id} className={bulletinClass}>
-            <div><img src={bulletin.image} /></div>
-            <div>
-                <div>
-                    <Link href={'/community/bulletins/' + bulletin.id}>
-                        <a className={'title'}>{bulletin.title}</a>
-                    </Link>
-                </div>
-                <div dangerouslySetInnerHTML={{ __html: content }} />
-            </div>
+            <div><Link href={'/community/bulletins/' + bulletin.id}><a><img src={bulletin.image} /></a></Link></div>
+            {bulletin.meta && bulletin.meta.image_only ? '': contentSection }
         </div>
     );
 }

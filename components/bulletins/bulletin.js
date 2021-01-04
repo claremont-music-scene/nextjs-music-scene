7ce17f372621205a3cbefbc6110583dae8cb60f3
Brian Luft <brian@lincolnloop.com>
2021-01-04 13:42:36 -0800

    Force img fit in bulletin list; display image on bulletin detail

--- components/bulletins/bulletin.js
+++ components/bulletins/bulletin.js
@@ -4,8 +4,11 @@ export default function Bulletin({ bulletin }) {
     const content = bulletin.content.replace(/\n/g, '<br/>');
     return (
         <div>
+            <style jsx>{`
+              div img { max-height: 100%; max-width: 100%}
+            `}</style>
             <h1>{bulletin.title}</h1>
-            {/* <div><img src={bulletin.imageUrl} /></div> */}
+            <div><img src={bulletin.image} /></div>
             <div dangerouslySetInnerHTML={{ __html: content }} />
             <div>
                 <Link href='/community/bulletins/'>

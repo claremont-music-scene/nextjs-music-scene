ea018aac53599aa9f7e06ee030325e292061f85c
Ben Brooks <ben@nutandem.com>
2021-01-06 15:21:29 -0800

    add layout: fullsize img w/ caption

--- components/bulletins/bulletin-card.js
+++ components/bulletins/bulletin-card.js
@@ -1,10 +1,10 @@
 import Router from 'next/router';
 
-
 export default function BulletinCard({ bulletin }) {
     const { meta, id, is_featured, featured_order, title, content, image } = bulletin,
+        metadata = meta ? id >= 18 ? JSON.parse(meta) : meta : null, // temporarily complex
         bulletinClass = 'grid-cell clickable' + (is_featured ? ' feat-' + featured_order : ''),
-        layout = meta && meta.image_only ? 'FullSizeImage(nobackgroundortext)' : meta && meta.layout || 'NoImage', // temporarily complex
+        layout = metadata && metadata.image_only ? 'FullSizeImage(nobackgroundortext)' : metadata && metadata.layout || 'NoImage', // temporarily complex
 
         onClickCard = () => {
             Router.push('/community/bulletins/' + id);
@@ -18,14 +18,25 @@ export default function BulletinCard({ bulletin }) {
 
     let layoutHtml = <></>;
 
-    console.log('🚀 ~ BulletinCard ~ layout', layout);
-
     switch (layout) {
         case 'FullSizeImage(nobackgroundortext)':
             layoutHtml = imageSection;
             break;
+
+        case 'Full-SizeImagewithTitleascaption':
+            layoutHtml =
+                <div className='overlay-wrap'>
+                    <div className='overlay-img'>
+                        {imageSection}
+                    </div>
+                    <div className='overlay-text'>
+                        {titleSection}
+                    </div>
+                </div>;
+            break;
+
         default:
-            layoutHtml = <>{imageSection} {titleSection} {contentSection}</>;
+            layoutHtml = <div>{imageSection} {titleSection} {contentSection}</div>;
     }
 
     return (

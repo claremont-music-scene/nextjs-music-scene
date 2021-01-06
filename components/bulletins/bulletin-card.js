4cc4d127407cd3d3e1a8ecb73c01112a6353c402
Ben Brooks <ben@nutandem.com>
2021-01-06 13:03:14 -0800

    start using meta layout; make cards clickable

--- components/bulletins/bulletin-card.js
+++ components/bulletins/bulletin-card.js
@@ -1,22 +1,28 @@
-import Link from 'next/link';
+// import Link from 'next/link';
+import Router from 'next/router';
+
 
 export default function BulletinCard({ bulletin }) {
-    const bulletinClass = 'grid-cell' + (bulletin.is_featured ? ' feat-' + bulletin.featured_order : ''),
-        content = bulletin.content.replace(/\n/g, '<br/>');
+    const bulletinClass = 'grid-cell clickable' + (bulletin.is_featured ? ' feat-' + bulletin.featured_order : ''),
+        content = bulletin.content.replace(/\n/g, '<br/>'),
+        { meta } = bulletin,
+        layout = meta && meta.image_only ? 'FullSizeImage(nobackgroundortext)' : meta && meta.layout || 'NoImage',
+        bulletinUrl = '/community/bulletins/' + bulletin.id,
 
-    const contentSection = (<div>
-        <div>
-            <Link href={'/community/bulletins/' + bulletin.id}>
-                <a className={'title'}>{bulletin.title}</a>
-            </Link>
-        </div>
-        <div dangerouslySetInnerHTML={{ __html: content }} />
-    </div>)
+        onClickCard = () => {
+            Router.push(bulletinUrl);
+        },
+
+        contentSection =
+            <div>
+                <div className={'title'}>{bulletin.title}</div>
+                <div dangerouslySetInnerHTML={{ __html: content }} />
+            </div>;
 
     return (
-        <div key={bulletin.id} className={bulletinClass}>
-            <div><Link href={'/community/bulletins/' + bulletin.id}><a><img src={bulletin.image} /></a></Link></div>
-            {bulletin.meta && bulletin.meta.image_only ? '': contentSection }
+        <div key={bulletin.id} className={bulletinClass} onClick={onClickCard}>
+            <div><img src={bulletin.image} /></div>
+            {layout !== 'FullSizeImage(nobackgroundortext)' && contentSection}
         </div>
     );
 }

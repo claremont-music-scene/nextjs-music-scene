4ecb08a504c1c5253aacb635e9cf38f39bc7aace
Ben Brooks <ben@nutandem.com>
2021-01-07 06:35:27 -0800

    Bulltn details use SSR to show new item after POST

--- pages/community/bulletins/[id].js
+++ pages/community/bulletins/[id].js
@@ -2,22 +2,9 @@ import SingleColumnLayout from '../../../components/layouts/single-column';
 import Bulletin from '../../../components/bulletins/bulletin';
 import { fetcher } from '../../../util/crud';
 
-export async function getStaticProps({ params }) {
+export async function getServerSideProps({ params }) {
     const post = await fetcher('https://music-scene-api.herokuapp.com/api/v1/bulletin_board/items/' + params.id);
     return { props: { post } };
-}
-
-export async function getStaticPaths() {
-    const posts = await fetcher('https://music-scene-api.herokuapp.com/api/v1/bulletin_board/items');
-
-    return {
-        paths: posts.map(post => ({
-            params: {
-                id: post.id.toString()
-            }
-        })),
-        fallback: false
-    };
 }
 
 const BulletinPage = ({ post }) => (

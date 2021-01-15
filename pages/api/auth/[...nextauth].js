2cf3f406149879e761b109665b05c4cc9937aa92
Brian Luft <brian@lincolnloop.com>
2021-01-14 20:49:00 -0800

    Fix options scoping; end-to-end auth with API working; session retrieval working server side

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
+import {authPoster} from "../../../util/crud";
 
 const options = {
     // Configure one or more authentication providers
@@ -8,101 +9,83 @@ const options = {
             //The Credentials provider can only be used if JSON Web Tokens are enabled
             // for sessions. Users authenticated with the Credentials provider are not persisted in the database.
 
-            debug: false,
+            authorize: async (credentials) => {
 
-            secret: "shiny",
-
-            // The name to display on the sign in form (e.g. 'Sign in with...')
-            name: 'your username',
-
+                // Add logic here to look up the user from the credentials supplied
+                const authRes = await authPoster(`${process.env.API_URL}/auth/token/login/`, {
+                    username: credentials.username,
+                    password: credentials.password
+                })
+                if (authRes && authRes.auth_token) {
+                    // Any object returned will be saved in `user` property of the JWT
+                    return Promise.resolve({apiToken: authRes.auth_token})
+                } else {
+                    // If you return null or false then the credentials will be rejected
+                    return Promise.resolve(null)
+                    // You can also Reject this callback with an Error or with a URL:
+                    // return Promise.reject(new Error('error message')) // Redirect to error page
+                    // return Promise.reject('/path/to/redirect')        // Redirect to a URL
+                }
+            },
             // The credentials is used to generate a suitable form on the sign in page.
             // You can specify whatever fields you are expecting to be submitted.
             // e.g. domain, username, password, 2FA token, etc.
             credentials: {
-                username: { label: "Username", type: "text", placeholder: "jimmy" },
-                password: {  label: "Password", type: "password" }
+                username: {label: "Username", type: "text", placeholder: "jimmy"},
+                password: {label: "Password", type: "password"}
             },
 
-            session: {
-                // Use JSON Web Tokens for session instead of database sessions.
-                // This option can be used with or without a database for users/accounts.
-                // Note: `jwt` is automatically set to `true` if no database is specified.
-                jwt: true,
-
-                // Seconds - How long until an idle session expires and is no longer valid.
-                maxAge: 30 * 24 * 60 * 60, // 30 days
-
-                // Seconds - Throttle how frequently to write to database to extend a session.
-                // Use it to limit write operations. Set to 0 to always update the database.
-                // Note: This option is ignored if using JSON Web Tokens
-                //updateAge: 24 * 60 * 60, // 24 hours
-            },
-
-            jwt: {
-                // A secret to use for key generation - you should set this explicitly
-                // Defaults to NextAuth.js secret if not explicitly specified.
-                secret: 'Stay away from my special lady friend',
-
-                // Set to true to use encryption. Defaults to false (signing only).
-                encryption: true,
-
-                // You can define your own encode/decode functions for signing and encryption
-                // if you want to override the default behaviour.
-                // encode: async ({ secret, token, maxAge }) => {},
-                // decode: async ({ secret, token, maxAge }) => {},
-            },
-
-            callbacks: {
-                jwt: async (token, user, account, profile, isNewUser) => {
-                    const isSignIn = user ? true : false;
-
-                    console.log('jwt callback', isSignIn)
-
-                    if (isSignIn) {
-                        token.apiToken = user.token;
-                    }
-                    return token;
-                },
-                session: async (session, user) => {
-                    return { ...session, user: { username: user.username } };
-                },
-            },
-
-            events: {
-                signIn: async (message) => { /* on successful sign in */ },
-                signOut: async (message) => { /* on signout */ },
-                createUser: async (message) => { /* user created */ },
-                linkAccount: async (message) => { /* account linked to a user */ },
-                session: async (message) => { /* session is active */ },
-                error: async (message) => { /* error in authentication flow */ }
-            },
-
-            authorize: async (credentials) => {
-
-
-                const user = (credentials) => {
-
-                    //TODO auth POST
-
-                    console.log('authenticating', credentials)
-
-                    // You need to provide your own logic here that takes the credentials
-                    // submitted and returns either a object representing a user or value
-                    // that is false/null if the credentials are invalid.
-                    // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
-                    return {token: "0123456789-oak"}
-                }
-
-
-                if (user) {
-                    // Any user object returned here will be saved in the JSON Web Token
-                    return Promise.resolve(user)
-                } else {
-                    return Promise.resolve(null)
-                }
-            }
         })
-    ]
+    ],
+
+    debug: false,
+
+    secret: process.env.SECRET,
+
+    session: {
+        // Use JSON Web Tokens for session instead of database sessions.
+        // This option can be used with or without a database for users/accounts.
+        // Note: `jwt` is automatically set to `true` if no database is specified.
+        jwt: true,
+
+        // Seconds - How long until an idle session expires and is no longer valid.
+        maxAge: 30 * 24 * 60 * 60, // 30 days
+
+        // Seconds - Throttle how frequently to write to database to extend a session.
+        // Use it to limit write operations. Set to 0 to always update the database.
+        // Note: This option is ignored if using JSON Web Tokens
+        //updateAge: 24 * 60 * 60, // 24 hours
+    },
+
+    jwt: {
+        // A secret to use for key generation - you should set this explicitly
+        // Defaults to NextAuth.js secret if not explicitly specified.
+        //secret: 'Stay away from my special lady friend',
+
+        // Set to true to use encryption. Defaults to false (signing only).
+        encryption: true,
+
+        // You can define your own encode/decode functions for signing and encryption
+        // if you want to override the default behaviour.
+        // encode: async ({ secret, token, maxAge }) => {},
+        // decode: async ({ secret, token, maxAge }) => {},
+    },
+
+    callbacks: {
+        jwt: async (token, user, account, profile, isNewUser) => {
+            const isSignIn = user ? true : false;
+            if (isSignIn) {
+                token.apiToken = user.apiToken;
+            }
+            return Promise.resolve(token);
+        },
+        session: async (session, user) => {
+            if (user.apiToken) {
+                session.apiToken = user.apiToken
+            }
+            return Promise.resolve(session);
+        }
+    }
 
 }
 
--- pages/community/bulletins/new.js
+++ pages/community/bulletins/new.js
@@ -10,19 +10,8 @@ export async function getStaticProps() {
 }
 
 
-
 export default function Page () {
     const [ session, loading ] = useSession()
-
-    // Fetch content from protected route
-    // useEffect(()=>{
-    //     const fetchData = async () => {
-    //         const res = await fetch('/api/examples/protected')
-    //         const json = await res.json()
-    //         if (json.content) { setContent(json.content) }
-    //     }
-    //     fetchData()
-    // },[session])
 
     // When rendering client side don't display anything until loading is complete
     if (typeof window !== 'undefined' && loading) return null

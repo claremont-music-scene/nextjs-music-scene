32d4ca5e19148aff11e7f87e977103fb385f459d
Brian Luft <brian@lincolnloop.com>
2021-01-21 20:06:31 -0800

    Adds a few lame events tests; upgrades cypress to latest

--- cypress/integration/events.js
+++ cypress/integration/events.js
@@ -3,3 +3,23 @@ describe('Events Page Loads', () => {
         cy.visit('http://localhost:3000/events')
     })
 })
+
+
+describe('Other Events Page Loads', () => {
+    it('loads the page and checks for event presence', () => {
+        cy.visit('http://localhost:3000/community/events')
+        cy.get('.event-card > :nth-child(1) > div > a').click()
+    })
+
+})
+
+describe('Load Event from Events Page', () => {
+    it('follows an event link to event detail', () => {
+        cy.visit('http://localhost:3000/community/events')
+        cy.get('.event-card > :nth-child(1) > div > a').click()
+
+        cy.get(':nth-child(6) > a').click()
+        cy.url().should('equal', 'http://localhost:3000/community/events')
+    })
+
+})
